da13e568a9991af7b524e0658bbe1c58ac0a9f89
Julia Alexeeva <julia4406@gmail.com>
2025-06-17 12:53:14 +0300

    Create database connection

---
+++ app/database.py
@@ -0,0 +1,22 @@
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, \
+    AsyncSession
+
+DATABASE_URL="sqlite+aiosqlite:///./lib.db"
+
+async_engine = create_async_engine(DATABASE_URL)
+async_session_maker = async_sessionmaker(
+    async_engine, expire_on_commit=False
+)
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        try:
+            yield session
+            await session.commit()
+        except Exception:
+            await session.rollback()
+            raise
+        finally:
+            await session.close()
