71a5e508ce0e5b0686aae58b88c3ad9f546e2b57
Julia Alexeeva <julia4406@gmail.com>
2025-06-17 12:53:26 +0300

    Create models

---
+++ app/models.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import (
+    Mapped,
+    mapped_column,
+    relationship,
+    DeclarativeBase
+)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Author(Base):
+    __tablename__ = "authors"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(unique=True)
+    bio: Mapped[str] = mapped_column()
+
+    books: Mapped[list["Book"]] = relationship(
+        back_populates="author",
+        cascade="all, delete-orphan"
+    )
+
+
+class Book(Base):
+    __tablename__ = "books"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column()
+    summary: Mapped[str] = mapped_column()
+    publication_date: Mapped[datetime] = mapped_column()
+    author_id: Mapped[int] = mapped_column(ForeignKey("authors.id"))
+
+    author: Mapped["Author"] = relationship(back_populates="books")
